17df28faf70f64cf15feee4d1f3d40c5779504e0
RPraanesh <45656825+RPraanesh@users.noreply.github.com>
2018-12-14 18:29:54 +0530

    Update JAva.java

--- src/main/java/com/java/test/JAva.java
+++ src/main/java/com/java/test/JAva.java
@@ -4,5 +4,19 @@ public class JAva {
 public static void main(String args[])
 {
 	System.out.print("Hello world");
+	String input = "GeeksforGeeks";
+	byte [] strAsByteArray = input.getBytes(); 
+	  
+    byte [] result =  
+               new byte [strAsByteArray.length]; 
+
+    // Store result in reverse order into the 
+    // result byte[] 
+    for (int i = 0; i<strAsByteArray.length; i++) 
+        result[i] =  
+         strAsByteArray[strAsByteArray.length-i-1]; 
+
+    System.out.println(new String(result)); 
+	
 }
 }
